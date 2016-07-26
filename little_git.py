bee92ac18474d4b9390d93bbff362dc75d5ea3d1
Charley Lewittes <ctlewitt@gmail.com>
2016-07-26 12:32:41 -0400

    added little_git to make backup of directory

---
+++ little_git.py
@@ -0,0 +1,10 @@
+# write a program that, given a directory name, makes a backup directory called name.bak
+# with copies of everythign under the original directory
+
+import shutil
+import os
+
+def little_git(src):
+    shutil.copytree(src, src+".bak")
+
+little_git("foo")

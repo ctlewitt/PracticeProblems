e24bf862c95de2fa8bf7345b299b5289dd509db8
Charley Lewittes <ctlewitt@gmail.com>
2016-07-27 15:08:29 -0400

    refactored timing functionality to use decorator

--- decorators_tutorial_timing.py
+++ decorators_tutorial_timing.py
@@ -5,7 +5,18 @@ import time
 def random_list(n):
     return [random.random() for _ in range(n)]
 
+def time_me(func):
+    def timed_funcion(list):
+        start = time.time()
+        func(list)
+        end = time.time()
+        total_time = end - start
+        print("sorting list of size {} with {} took {} milliseconds".format(
+            len(list), func.__name__, total_time * 1000)
+        )
+    return timed_funcion
 
+@time_me
 def insertion_sort(xs):
     result = []
     for x in xs:
@@ -15,11 +26,14 @@ def insertion_sort(xs):
             for i, r in enumerate(result+[float("inf")]):
                 if r > x:
                     break
-            result.insert(i, x)
+                result.insert(i, x)
     return result
 
-
+@time_me
 def quick_sort(xs):
+    return quicksort(xs)
+
+def quicksort(xs):
     if len(xs) <= 1:
         return xs
     pivot = xs[0]
@@ -30,20 +44,17 @@ def quick_sort(xs):
             left.append(x)
         else:
             right.append(x)
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    return quicksort(left) + [pivot] + quicksort(right)
 
 
 # task: refactor this by writing a decorator for printing out timing
 # information and annotating the sorting functions above with it, such
 # we don't have to have any timing related code in the loop below
 
-def measure_timings():
-    for sort_function in [insertion_sort, quick_sort]:
+def measure_timings(sort_functions):
+    for sort_function in sort_functions:
         for size in [10, 100, 1000, 10000]:
-            start = time.time()
             sort_function(random_list(size))
-            end = time.time()
-            total_time = end - start
-            print( "sorting list of size {} with {} took {} milliseconds".format(
-                size, sort_function.__name__, total_time*1000)
-            )
+
+
+measure_timings([quick_sort, insertion_sort])

2067d85401eed50edc35ea91144eebb80f05fde6
Charley Lewittes <ctlewitt@gmail.com>
2016-08-15 16:46:37 -0400

    implemented recursive solution to bitwise multiplication

--- mult_bitwise.py
+++ mult_bitwise.py
@@ -1,4 +1,4 @@
-def bitwise_multiply(x, y):
+def mult(x, y):
     prod = 0
     bits_to_shift = 0
     shifted_y = y
@@ -9,7 +9,28 @@ def bitwise_multiply(x, y):
         shifted_y >>= 1
     return prod
 
-print(bitwise_multiply(2, 3))
-print(bitwise_multiply(0, 5))
-print(bitwise_multiply(2, 0))
-print(bitwise_multiply(57, 98))
+
+def mult_rec(x, y):
+    return mult_rec_aux(x, y, 0, 0)
+
+
+def mult_rec_aux(x, shifted_y, bits_to_shift, prod):
+    if shifted_y == 0:
+        return prod
+    return mult_rec_aux(x, shifted_y >> 1, bits_to_shift + 1, prod + (x << bits_to_shift) if shifted_y & 1 else prod)
+
+
+print("testing iterative program")
+print(mult_rec(3, 15))
+print(mult (2, 3))
+print(mult (0, 5))
+print(mult (2, 0))
+print(mult (57, 98))
+
+
+print("testing recursive program")
+print(mult_rec(3, 15))
+print(mult_rec (2, 3))
+print(mult_rec (0, 5))
+print(mult_rec (2, 0))
+print(mult_rec (57, 98))

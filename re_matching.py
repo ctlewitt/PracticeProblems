2a954dcb058bb5773da8474f0c17bc6fc650232d
Charley Lewittes <ctlewitt@gmail.com>
2016-08-24 16:10:54 -0400

    created Solution object/class

--- re_matching.py
+++ re_matching.py
@@ -1,38 +1,38 @@
 # https://leetcode.com/problems/regular-expression-matching/
 
-
-def isMatch(string, regex):
-    if regex == "":
-        return string == ""
-    if string == "":
-        return lookahead(regex) == "*" and len(regex) == 2
-    if regex[0] == ".":
-        if lookahead(regex) == "*":
-            # . occurs no times or 1 or more times
-            return isMatch(string, regex[2:]) or isMatch(string[1:], regex)
+class Solution(object):
+    def isMatch(self, string, regex):
+        if regex == "":
+            return string == ""
+        if string == "":
+            return self.lookahead(regex) == "*" and len(regex) == 2
+        if regex[0] == ".":
+            if self.lookahead(regex) == "*":
+                # . occurs no times or 1 or more times
+                return self.isMatch(string, regex[2:]) or self.isMatch(string[1:], regex)
+            else:
+                return self.isMatch(string[1:], regex[1:])
         else:
-            return isMatch(string[1:], regex[1:])
-    else:
-        if lookahead(regex) == "*":
-            # first char in string occurs no times or 1 or more times
-            return (string[0] == regex[0] and isMatch(string[1:], regex)) or isMatch(string, regex[2:])
-        else:
-            return string[0] == regex[0] and isMatch(string[1:], regex[1:])
+            if self.lookahead(regex) == "*":
+                # first char in string occurs no times or 1 or more times
+                return self.isMatch(string, regex[2:]) or (string[0] == regex[0] and self.isMatch(string[1:], regex))
+            else:
+                return string[0] == regex[0] and self.isMatch(string[1:], regex[1:])
 
+    def lookahead(self, regex):
+        try:
+            return regex[1]
+        except IndexError:
+            return None
 
-def lookahead(regex):
-    try:
-        return regex[1]
-    except IndexError:
-        return None
-
-print(isMatch("aa", "a"))
-print(isMatch("aa", "aa"))
-print(isMatch("aaa", "aa"))
-print(isMatch("aa", "a*"))
-print(isMatch("aa", ".*"))
-print(isMatch("ab", ".*"))
-print(isMatch("aab", "c*a*b"))
-print(isMatch("ab", ".*c"))
-print(isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c"))
+doer = Solution()
+print(doer.isMatch("aa", "a"))
+print(doer.isMatch("aa", "aa"))
+print(doer.isMatch("aaa", "aa"))
+print(doer.isMatch("aa", "a*"))
+print(doer.isMatch("aa", ".*"))
+print(doer.isMatch("ab", ".*"))
+print(doer.isMatch("aab", "c*a*b"))
+print(doer.isMatch("ab", ".*c"))
+print(doer.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c"))
 
